df7a4acf493c5cb3684859f0cb06ce28a4f35e81
Garrett Potts <gpotts@radiantblue.com>
2018-08-23 10:29:29 -0400

    Alloowing toJson without a mapping

--- plugins/omar-core-plugin/grails-app/controllers/omar/core/CoreUrlMappings.groovy
+++ plugins/omar-core-plugin/grails-app/controllers/omar/core/CoreUrlMappings.groovy
@@ -4,7 +4,8 @@ class CoreUrlMappings {
 
    static mappings = {
 
-     "/api/$action?/$id?"(controller: "apiDoc", action: "getDocuments")
+//     "/api/$action?/$id?"(controller: "apiDoc", action: "getDocuments")
+     "/api"(controller: "apiDoc", action: "getDocuments")
      "/apiDoc/index"(controller: "apiDoc", action: "getDocuments")
 
       "/$controller/$action?/$id?(.$format)?"{
