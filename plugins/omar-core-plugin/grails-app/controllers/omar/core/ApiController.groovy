6879ad7e8a68b354db09f9e49371380cc5c16d33
Garrett Potts <gpotts@radiantblue.com>
2018-08-23 10:29:00 -0400

    Added toJson for convenience

--- plugins/omar-core-plugin/grails-app/controllers/omar/core/ApiController.groovy
+++ plugins/omar-core-plugin/grails-app/controllers/omar/core/ApiController.groovy
@@ -1,6 +1,11 @@
 package omar.core
+import swagger.SwaggerService
 
 class ApiController {
-
+    def swaggerService
     def index() { }
+    def toJson()
+    {
+        render text:swaggerService.getJsonDocument(swaggerService.scanSwaggerResources())
+    }
 }
