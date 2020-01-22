80eaa56e502d94ab5c9ead40149001c21d0a2684
Kevin Naquin <kevin.naquin@digitalglobe.com>
2020-01-22 16:41:00 -0700

    Updating GZIP

--- plugins/omar-core-plugin/src/main/groovy/omar/core/OmarWebUtils.groovy
+++ plugins/omar-core-plugin/src/main/groovy/omar/core/OmarWebUtils.groovy
@@ -13,31 +13,14 @@ class OmarWebUtils
 	 * @param buffer The input string
 	 * @return The encoded, gzipped string
 	 */
-	static ByteArrayOutputStream gzippify(String buffer /*, String charset = StandardCharsets.UTF_8.name()*/ ){
+	static ByteArrayOutputStream gzippify( String buffer, String charset = StandardCharsets.UTF_8.name() ){
 		ByteArrayOutputStream targetStream = new ByteArrayOutputStream()
 		GZIPOutputStream gzipStream = new GZIPOutputStream(targetStream)
-		gzipStream.write(buffer.getBytes(/*charset*/))
+		gzipStream.write(buffer.getBytes( charset ))
 		gzipStream.close()
 
-		//byte[] zippedBytes = targetStream.toByteArray()
-		//targetStream.close()
+		targetStream.close()
 
-		//return zippedBytes.encodeBase64().toString()
 		return targetStream
-	}
-
-	/**
-	 * Gzippify an ArrayList by gzipping its toString
-	 * @param list
-	 * @return
-	 */
-	//static String gzippify(ArrayList list, String charset = StandardCharsets.UTF_8.name()){
-	//	return gzippify(list.toString(), charset)
-	//}
-
-	static String unGzippify(String buffer, String charset = StandardCharsets.UTF_8.name()) {
-		GZIPInputStream inflaterStream = new GZIPInputStream(new ByteArrayInputStream(buffer.decodeBase64()))
-		String uncompressedStr = inflaterStream.getText(charset)
-		return uncompressedStr
 	}
 }
